b65aaf06a241d21d3eb30b99efb14a099cdcde46
david-newspark <125764120+david-newspark@users.noreply.github.com>
2023-04-11 21:05:48 -0700

    Test build (#17)

---
+++ frontend/src/components/Logs/LogTableIconDialog.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import LogTable from '@/components/Logs/LogTable.vue';
+
+const dialog = ref(false)
+</script>
+<template>
+    <div class="text-center">
+        <v-btn variant="text" :ripple="false">
+            <v-icon icon="mdi-clipboard-text-outline"></v-icon>
+            <v-dialog v-model="dialog" activator="parent" width="auto">
+                <v-card>
+                    <v-card-text>
+                        <LogTable />
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-btn>
+    </div>
+</template>
